9d691f1e2a1eeb0ecbdd54061377fe85c6977c8d
Deependra Patel <pateldeependra06@gmail.com>
2015-01-21 14:45:35 +0530

    work

--- lab2/main.cc
+++ lab2/main.cc
@@ -6,6 +6,21 @@ int main (int argc, char** arg)
 {
   Parser parser;
   parser.parse();
+  Scanner scanner;   // define a Scanner object
+  while (int token = scanner.lex()) // get all tokens
+    {
+      string const &text = scanner.matched();
+      switch (token){
+		case Parser::MNEMONIC:
+		  cout << "mnemonic: " << text << '\n';
+		  break;
+		case Parser::NUMBER:
+			cout<<"number: "<< text <<endl;
+			break;
+		default:
+			cout<<"Other: "<< text<<endl;
+		}
+	}
 }
 
 
